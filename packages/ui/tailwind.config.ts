dfbba4d532b8d2d8b20c5a6e8cc2c209fb98cff8
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-09-30 20:35:31 +0200

    feat: remove incorrect shadcn components && fix settings

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,39 @@
+/*
+ * This file is not used for any compilation purpose, it is only used
+ * for Tailwind Intellisense & Autocompletion in the source files
+ */
+import type { Config } from "tailwindcss";
+
+import baseConfig from "@acme/tailwind-config/web";
+
+export default {
+  darkMode: ["class"],
+  content: ["./src/**/*.tsx"],
+  presets: [baseConfig],
+  theme: {
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+} satisfies Config;
