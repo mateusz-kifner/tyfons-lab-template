51a1100d3e8f8c48d9110e81fa1a6d9ef4f6f241
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-10-15 22:02:24 +0200

    feat: add date structure validation to date field

--- apps/nextjs/src/pages/test/date-picker.tsx
+++ apps/nextjs/src/pages/test/date-picker.tsx
@@ -1,35 +1,75 @@
 "use client";
 
 import * as React from "react";
-import { format } from "date-fns";
-import { IconCalendar } from "@tabler/icons-react";
-
-import { cn } from "@acme/ui";
-import { Button } from "@acme/ui/button";
+import { format, parse, parseISO } from "date-fns";
 import { Calendar } from "@acme/ui/calendar";
-import {
-  Popover,
-  PopoverAnchor,
-  PopoverContent,
-  PopoverTrigger,
-} from "@acme/ui/popover";
+import { Popover, PopoverAnchor, PopoverContent } from "@acme/ui/popover";
 import { Input } from "@acme/ui/input";
 import { useState } from "react";
 import { useClickOutside } from "@mantine/hooks";
+
+const plDateRegex = /^(0{0,1}[1-9]|[12]\d|3[01])\.(0{0,1}[1-9]|1[0-2])\.\d{4}$/;
+const isoDateOnlyRegex =
+  /^\d{4}-(0{0,1}[1-9]|1[0-2])-(0{0,1}[1-9]|[12]\d|3[01])$/;
+
+function convertDateWithValidation(date_str: string) {
+  let warning = false;
+  let error = false;
+
+  if (!(plDateRegex.test(date_str) || isoDateOnlyRegex.test(date_str)))
+    warning = true;
+
+  const date = convertToDate(date_str);
+  if (date === undefined) error = true;
+
+  return { date, warning, error };
+}
+
+function convertToDate(dateStr: string) {
+  let date: Date;
+
+  try {
+    date = parseISO(dateStr);
+    if (!Number.isNaN(date.getTime())) {
+      return date;
+    }
+  } catch (error) {
+    //noop
+  }
+
+  try {
+    date = parse(dateStr, "dd.MM.yyyy", new Date());
+    if (!Number.isNaN(date.getTime())) {
+      return date;
+    }
+  } catch (error) {
+    return undefined;
+  }
+  return undefined;
+}
 
 export function DatePickerDemo() {
   const [date, setDate] = React.useState<string>("");
   const [open, setOpen] = useState(false);
   const ref = useClickOutside(() => setOpen(false));
 
+  const { date: date_obj, warning, error } = convertDateWithValidation(date);
+
   return (
-    <div ref={ref}>
+    <div ref={ref} className="p-6">
       <Popover open={open}>
         <PopoverAnchor asChild>
           <Input
             value={date}
             onChange={(e) => setDate(e.target.value)}
             onFocus={() => setOpen(true)}
+            className={
+              warning
+                ? "focus-visible:ring-yellow-500"
+                : error
+                  ? "focus-visible:ring-destructive"
+                  : undefined
+            }
           />
         </PopoverAnchor>
         <PopoverContent
@@ -40,9 +80,9 @@ export function DatePickerDemo() {
         >
           <Calendar
             mode="single"
-            selected={date ? new Date(date) : new Date()}
+            selected={date_obj}
             onSelect={(date) =>
-              date ? setDate(format(date, "yyyy-MM-dd")) : setDate("")
+              date ? setDate(format(date, "dd.MM.yyyy")) : setDate("")
             }
           />
         </PopoverContent>
@@ -50,3 +90,5 @@ export function DatePickerDemo() {
     </div>
   );
 }
+
+export default DatePickerDemo;
